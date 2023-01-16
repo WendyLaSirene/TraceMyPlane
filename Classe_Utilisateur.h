248fdc9f3154d1a33af8c52ae303ca31dcbfafaa
Metalion18 <phoenixus.titan@gmail.Com>
2023-01-16 14:04:48 +0100

    fin de la page Login

--- Classe_Utilisateur.h
+++ Classe_Utilisateur.h
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <QFile>
+#include <QTextStream>
 using namespace std ;
 
 
@@ -10,38 +12,53 @@ class Utilisateur
 {
 	private :
 	    int ID ;
-	    string Nom ;
-	    string Mot_De_Passe ;
+        QString Nom ;
+        QString Mot_De_Passe ;
 	    
 	public :
 		Utilisateur () ;
-		Utilisateur ( int i , string Name , string MDP ) 
+        Utilisateur ( int i , QString Name , QString MDP )
 		{
 	        ID = i ;
 	        Nom = Name ;
 	        Mot_De_Passe = MDP ;
 		}
 		void Set_ID ( int i ) { ID = i ; }
-		void Set_Nom ( string n ) { Nom = n ; }
-		void Set_MDP ( string MDP ) { Mot_De_Passe = MDP ; }
-		string Get_Nom () { return Nom ; }
-		bool Check_MDP ( string MDP_Candidat ) { return MDP_Candidat == Mot_De_Passe ; }
+        void Set_Nom ( QString n ) { Nom = n ; }
+        void Set_MDP ( QString MDP ) { Mot_De_Passe = MDP ; }
+        QString Get_Nom () { return Nom ; }
+        bool Check_MDP ( QString MDP_Candidat ) { return MDP_Candidat == Mot_De_Passe ; }
 		// La fonction "Get_MDP" n'existe pas et c'est normal : on ne veut pas que n'importe qui recupere les mots de passe !
 		
-		void Afficher_Tout () { cout << "ID : / " << ID << " / Nom : " << Nom << " / MDP : " << Mot_De_Passe ; }
+        //void Afficher_Tout () { cout << "ID : / " << ID << " / Nom : " << Nom << " / MDP : " << Mot_De_Passe ; }
 } ;
 
 
-int Charger_Liste_Utilisateurs ( string Nom_Fichier , vector<Utilisateur> &U )
+bool Charger_Liste_Utilisateurs ( QString Nom_Fichier , vector<Utilisateur> &U )
 {
-    string Contenu ;
+    /*string Contenu ;
     string Ligne ;
     string Mot ;
-    vector<string> Ligne_Dissociee ;
-    
-    int n ;
+    vector<string> Ligne_Dissociee ;*/
+
+    QString line;
+    QStringList line_Separed;
+    bool okToInt;
 	
-    stringstream ss ;
+    QFile file(Nom_Fichier);
+        if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+            return false;
+
+        QTextStream in(&file);
+        in.readLine();// 1ere ligne est a jeter : en-tete
+        while (!in.atEnd()) {
+            line = in.readLine();
+            line_Separed = line.split(',');
+            U.push_back(Utilisateur(line_Separed[2].toInt(&okToInt,10),line_Separed[0],line_Separed[1]));
+        }
+        return true;
+
+    /*stringstream ss ;
     
     ifstream Fichier ( Nom_Fichier ) ;
 	if ( ! Fichier.is_open() )
@@ -65,5 +82,22 @@ int Charger_Liste_Utilisateurs ( string Nom_Fichier , vector<Utilisateur> &U )
         Ligne_Dissociee.clear() ;
 	}
     
-    return 0 ;
+    return 0 ;*/
+}
+
+//verifie si la combinaison nom-mot de passe est correct. renvoie 0 s'il y as eu une erreur d'ouvertur du fichier,
+//1 si l'utilisateur n'as pas été trouver et 2 s'il est bon mais avec un mauvais Mot de passe et 3 si tout est bon
+int correctEntry(QString nameEnter, QString mdpEnter){
+    vector<Utilisateur> vectorUsers;
+    if(Charger_Liste_Utilisateurs(":/sourceClasses/Users.csv",vectorUsers)){
+        for (vector<Utilisateur>::const_iterator i=vectorUsers.begin(); i != vectorUsers.end(); i++) {
+            Utilisateur userToCompar = *i;
+            if(nameEnter==userToCompar.Get_Nom()){
+                if (userToCompar.Check_MDP(mdpEnter)){
+                    return 3;//utilisateur trouvé avec le bon mot de passe
+                }else return 2;//utilisateur trouvé mais mauvais mot de passe
+            }
+        }
+        return 1;//utilisateur non trouvé
+    }else return 0;//erreur lors de l'ouverture du fichier
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "Classe_Utilisateur.h"
+#include <QDesktopServices>
+#include <QUrl>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -20,17 +23,30 @@ void MainWindow::on_btn_Connexion_clicked()
   name=ui->lineEdit_Name->text();
   pswrd=ui->lineEdit_MdP->text();
 if(name==""||pswrd==""){
-    QMessageBox::warning(this,"Warning entry","veuillez entrer votre nom et votre mot de passe");
+    QMessageBox::warning(this,"Warning entry","Veuillez entrer votre nom et votre mot de passe");
     return;
 }
-this->hide();
-Accueil *accueil = new Accueil();
-accueil->show();
+switch (correctEntry(name,pswrd)) {
+case 0://erreur lors de l'ouvertur du fichier
+    QMessageBox::warning(this,"Critical Error","Une erreur est survenue lors de la vérification, merci de contacter le service client");
+    break;
+case 1://utilisateur non trouvé
+    QMessageBox::warning(this,"Warning entry","Utilisateur non trouvé, réessayez ou inscrivez-vous");
+    break;
+case 2://utilisateur trouvé mais mauvais mot de passe
+    QMessageBox::warning(this,"Warning entry","Mot de passe saisie incorrect. Attention aux majuscules");
+    break;
+case 3://utilisateur trouvé avec le bon mot de passe
+    this->hide();
+    Accueil *accueil = new Accueil();
+    accueil->show();
+    break;
+}
 }
 
-
+//bouton créer un compte renvoie sur une page web pour le contrôle de la création d'un compte
 void MainWindow::on_btn_CreatCount_clicked()
 {
-
+    QDesktopServices::openUrl(QUrl("https://www.google.fr"));
 }
 
